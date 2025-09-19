dc21c29b92e20543a4457d2cb301082ef359d9fd
Till Hoffmann <till@thetillhoff.de>
2025-09-19 15:22:34 +0200

    Update webscan formula & github action to update the version automatically

--- Formula/webscan.rb
+++ Formula/webscan.rb
@@ -1,81 +1,38 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-
 class Webscan < Formula
   desc "Verifies web things"
   homepage "https://github.com/thetillhoff/webscan"
   license "MIT"
+  version "<no value>"
 
-  depends_on "go" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/yourorg/webscan/releases/download/v#{version}/webscan_darwin_arm64"
+      sha256 "<no value>"
+    end
 
-  # version = git describe --exact-match --tags
-
-  url "https://github.com/thetillhoff/webscan/archive/refs/tags/$(version)).tar.gz"
-  sha256 "9a9e5a6967dc5cfb58b167fa99762222ffc90089a92fdc3d23c04b4eee6301fd"
-
-  # on_macos do
-  #   if Hardware::CPU.intel?
-  #     # macos x86_64
-  #     url "https://github.com/thetillhoff/webscan/releases/download/v2.0.2/webscan_darwin_arm64", using:CurlDownloadStrategy
-  #     sha256 "9a9e5a6967dc5cfb58b167fa99762222ffc90089a92fdc3d23c04b4eee6301fd"
-      
-  #     def install
-  #       bin.install "webscan"
-  #     end
-  #   end
-
-  #   if Hardware::CPU.arm?
-  #     # darwin arm64
-  #     url "https://github.com/thetillhoff/webscan/releases/download/v2.0.2/webscan_darwin_arm64", using:CurlDownloadStrategy
-  #     sha256 ""
-     
-  #     def install
-  #       bin.install "webscan"
-  #     end
-  #   end
-
-  # on_linux do
-  #   if Hardware::CPU.intel?
-  #     # linux x86_64
-  #     url "https://github.com/thetillhoff/webscan/releases/download/v2.0.2/webscan_darwin_arm64", using:CurlDownloadStrategy
-  #     sha256 ""
-      
-  #     def install
-  #       bin.install "webscan"
-  #     end
-  #   end
-
-  #   if Hardware::CPU.arm?
-  #     if Hardware::CPU.is_64_bit?
-  #       # linux arm64
-  #       url "https://github.com/thetillhoff/webscan/releases/download/v2.0.2/webscan_darwin_arm64", using:CurlDownloadStrategy
-  #       sha256 ""
-      
-  #       def install
-  #         bin.install "webscan"
-  #       end
-  #     end
-
-  #     # if !Hardware::CPU.is_64_bit?
-  #     #   # linux armv7
-  #     #   url "https://github.com/thetillhoff/webscan/releases/download/v2.0.2/webscan_darwin_arm64", using:CurlDownloadStrategy
-  #     #   sha256 ""
-      
-  #     #   def install
-  #     #     bin.install "webscan"
-  #     #   end
-  #     # end
-  #   end
-
-  def install
-    system "make", "build"
-    bin.install "webscan"
-    
-    generate_completions_from_executable(bin/"webscan", "completion")
+    on_intel do
+      url "https://github.com/yourorg/webscan/releases/download/v#{version}/webscan_darwin_amd64"
+      sha256 "<no value>"
+    end
   end
 
-  # `test do` will create, run in and delete a temporary directory.
+  on_linux do
+    on_arm do
+      url "https://github.com/yourorg/webscan/releases/download/v#{version}/webscan_linux_arm64"
+      sha256 "<no value>"
+    end
+
+    on_intel do
+      url "https://github.com/yourorg/webscan/releases/download/v#{version}/webscan_linux_amd64"
+      sha256 "<no value>"
+    end
+  end
+
+  def install
+    bin.install "webscan"
+  end
+
   test do
-    system "#{bin}/webscan", "version"
+    assert_match "#{version}", shell_output("#{bin}/webscan --version")
   end
 end
