62070e6640b5cdb75f4ea675131a2a2a50d45fae
Till Hoffmann <till@thetillhoff.de>
2025-09-19 15:41:59 +0200

    Add template and use it in pipeline

---
+++ template/webscan.rb
@@ -0,0 +1,38 @@
+class Webscan < Formula
+  desc "Verifies web things"
+  homepage "https://github.com/thetillhoff/webscan"
+  license "MIT"
+  version "{{ .version }}"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/yourorg/webscan/releases/download/#{version}/webscan_darwin_arm64"
+      sha256 "{{ .macos_arm_sha }}"
+    end
+
+    on_intel do
+      url "https://github.com/yourorg/webscan/releases/download/#{version}/webscan_darwin_amd64"
+      sha256 "{{ .macos_intel_sha }}"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/yourorg/webscan/releases/download/#{version}/webscan_linux_arm64"
+      sha256 "{{ .linux_arm_sha }}"
+    end
+
+    on_intel do
+      url "https://github.com/yourorg/webscan/releases/download/#{version}/webscan_linux_amd64"
+      sha256 "{{ .linux_intel_sha }}"
+    end
+  end
+
+  def install
+    bin.install "webscan"
+  end
+
+  test do
+    assert_match "#{version}", shell_output("#{bin}/webscan --version")
+  end
+end
