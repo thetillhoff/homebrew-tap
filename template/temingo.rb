2c23ea257a60d974cafe2aab1fee5e7d23fb81fe
Till Hoffmann <till@thetillhoff.de>
2025-11-20 21:01:58 +0100

    fix description & add template for temingo

--- template/temingo.rb
+++ template/temingo.rb
@@ -2,29 +2,29 @@ class Temingo < Formula
   desc "Minimal golang templater for websites"
   homepage "https://github.com/thetillhoff/temingo"
   license "MIT"
-  version "v1.0.0"
+  version "${LATEST_VERSION}"
 
   on_macos do
     on_arm do
       url "#{homepage}/releases/download/#{version}/temingo_darwin_arm64"
-      sha256 "badsha"
+      sha256 "${MACOS_ARM_SHA}"
     end
 
     on_intel do
       url "#{homepage}/releases/download/#{version}/temingo_darwin_amd64"
-      sha256 "badsha"
+      sha256 "${MACOS_INTEL_SHA}"
     end
   end
 
   on_linux do
     on_arm do
       url "#{homepage}/releases/download/#{version}/temingo_linux_arm64"
-      sha256 "badsha"
+      sha256 "${LINUX_ARM_SHA}"
     end
 
     on_intel do
       url "#{homepage}/releases/download/#{version}/temingo_linux_amd64"
-      sha256 "badsha"
+      sha256 "${LINUX_INTEL_SHA}"
     end
   end
 
@@ -34,6 +34,6 @@ class Temingo < Formula
   end
 
   test do
-    assert_match "#{version}", shell_output("#{bin}/temingo version")
+    assert_match "#{version}", shell_output("#{bin}/temingo --version")
   end
 end
